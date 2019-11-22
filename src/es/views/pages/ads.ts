4cccf675f2fa5fa7d9d3613098053233961e70be
whitesunset <chester.work@gmail.com>
2019-11-22 23:05:38 +0100

    Ads page updates

--- src/es/views/pages/ads.ts
+++ src/es/views/pages/ads.ts
@@ -1,20 +1,67 @@
 export default {
   title: 'Advertising',
+  title1: 'Website',
   text1: `
-    <p>🍃<b>Minterscan</b> is a most popular Minter blockchain explorer.</p>
-    <p>Over 130 000 page views and more than 9 000 unique visitors per month.</p>
+    <p>
+      🍃<b> Minterscan</b> is a most popular Minter blockchain explorer.
+      <br>
+      Over 130 000 page views and more than 9 000 unique visitors per month.
+    </p>
     <p>
       Advertising on Minterscan is an opportunity for projects and teams
       to reach tens of thousands Minter users through sponsored content.
-      Attract Minter audience attention with the unobtrusive ads 
+      Attract Minter audience attention with the unobtrusive ads
       displayed at the top of Minterscan pages.
     </p>
     <p>
-      To advertise your project, please 
+      To advertise your project, please
       <a href="{url}">fill the form</a>.
     </p>
+    <p>
+      Advertising cost is <b>200 MSCAN</b> per 10% slot.
+      Promotional materials are displayed in rotation,
+      from one advertiser no more than 5 slots (50%) of the total number of impressions.
+    </p>
   `,
+  example: 'Example',
+  requirements: `
+    <ul>
+      <li>PNG icon, 48x48 pixels</li>
+      <li>Title, up to 20 symbols</li>
+      <li>Text, up to 100 symbols</li>
+    </ul>
+  `,
+  title2: 'Telegram',
   text2: `
+    <p>🍃<b> Minterscan</b> in Telegram is a quality posts on relevant topics.</p>
+    <p>
+      <a href="https://t.me/minterscan" taget="_blank">Russian channel</a> — 
+      <b>900</b> real subscribers, post coverage ~ <b>1 900</b>, ERR ~ <b>200%</b>.
+      Actual statistics available on 
+      <a href="https://tgstat.ru/channel/@minterscan" target="_blank">Tgstat.ru</a>
+      <br>
+      <a href="https://t.me/minterscan_en" taget="_blank">English channel</a> — 
+      <b>80</b> real subscribers, post coverage ~ <b>160</b>, ERR ~ <b>140%</b>.
+      Actual statistics available on
+      <a href="https://tgstat.ru/channel/@minterscan_en" target="_blank">Tgstat.ru</a>
+    </p>
+    <p>
+      In most cases, we edit advertising posts in accordance with our style.
+      We do not publish reposts, do not publish messages written by CAPS and do not publish
+      messages in which every second character is emoji. The channel is read by living people who
+      know our format and appreciate it. If this doesn’t suit you,
+      find another channel for advertising.
+    </p>
+    <p>
+      Advertising cost in
+      <a href="https://t.me/minterscan" taget="_blank">russian channel</a>
+      — <b>100 MSCAN</b>. 
+      Advertising cost in
+      <a href="https://t.me/minterscan_en" taget="_blank">english channel</a>
+      — <b>10 MSCAN</b>.
+    </p>
+  `,
+  disclaimer: `
     <p>
       We do not advertise poor quality projects and reserve
       the right to refuse to advertise without giving reasons.
